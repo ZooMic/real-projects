6d15a70f635a6613ba093186705c706808471ec6
Marcin Lichota <marcin.lichota@gmail.com>
2020-03-10 22:02:32 +0100

    quizit: Some fixes

--- quizit/src/components/Modal/Modal.js
+++ quizit/src/components/Modal/Modal.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { wrapper, modal, typeSign, content } from './Modal.module.css';
+import { wrapper, modal, content, /*typeSignBackground, typeSign, warning, info, error, success*/  } from './Modal.module.css';
 
-const Modal = ({ type, children }) => {
-    return (
+// const selectClassByType = type => {
+//     switch (type) {
+//         case 'warning': return warning;
+//         case 'info': return info;
+//         case 'error': return error;
+//         case 'success': return success;
+//         default: return info;
+//     }
+// }
+
+const Modal = ({ type = 'warning', children, visible = false }) => {
+    // const [sign, setSign] = useState(selectClassByType(type))
+    // useEffect(() => {
+    //     setSign(selectClassByType(type))
+    // }, [type])
+
+    return visible ?
         <div className={wrapper} >
             <div className={modal}>
-                <div className={typeSign}></div>
-                MODAL AS FUCK
+                {/* <div className={typeSignBackground}>
+                    <div className={`${typeSign} ${sign}`} />
+                </div> */}
+                <div className={content}>
+                    {children}
+                </div>
             </div>
-        </div>
-    )
+        </div> : null;
 }
 
 export default Modal;
